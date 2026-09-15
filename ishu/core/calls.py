cb368e0315f05d66cccc636c7e33ac725407fffc
Nishkarsh Kumar <nishkarshk46@gmail.com>
2026-09-15 17:32:15 +0530

    fix: remove invalid -bufsize decoding option from ffmpeg stream parameters

--- ishu/core/calls.py
+++ ishu/core/calls.py
@@ -222,7 +222,7 @@ class TgCall(PyTgCalls):
                         else types.MediaStream.Flags.IGNORE
                     ),
                     ffmpeg_parameters=(
-                        f"-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 2 -probesize 32k -analyzeduration 0 -bufsize 32k"
+                        f"-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -probesize 128k -analyzeduration 100000"
                         + (f" -ss {seek_time}" if seek_time > 1 else "")
                     ) if real_media_path.startswith("http") else (
                         f"-ss {seek_time}" if seek_time > 1 else None
@@ -294,7 +294,7 @@ class TgCall(PyTgCalls):
                         else types.MediaStream.Flags.IGNORE
                     ),
                     ffmpeg_parameters=(
-                        f"-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 2 -probesize 32k -analyzeduration 0 -bufsize 32k"
+                        f"-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -probesize 128k -analyzeduration 100000"
                         + (f" -ss {seek_time}" if seek_time > 1 else "")
                     ) if real_media_path.startswith("http") else (
                         f"-ss {seek_time}" if seek_time > 1 else None
